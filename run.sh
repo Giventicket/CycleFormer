115b74cab4a82c9d137f9567bf119cbbf2abdd69
junpyoseo <jpseo99@naver.com>
2024-05-24 03:56:33 +0000

    add

--- run.sh
+++
@@ -1,2 +0,0 @@
-# python3 train_and_inference.py --config ./configs/config_oneway.yaml
-python3 train_and_inference.py --config ./configs/config_twoway.yaml
--- run_experiments.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# python3 train_grid.py --config ./configs/config1.yaml
-python3 train_grid.py --config ./configs/config2.yaml
-python3 train_grid.py --config ./configs/config3.yaml
-python3 train_grid.py --config ./configs/config4.yaml
